48f9c58d5c1428c49dcb1e64df7615e82de3ad9c
Ebenezer Buabeng <ebenezer.buabeng@external.telekom.de>
2025-03-16 08:46:29 +0000

    feat: initial commit

---
+++ src/main/java/com/khaydev/App.java
@@ -0,0 +1,13 @@
+package com.khaydev;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
